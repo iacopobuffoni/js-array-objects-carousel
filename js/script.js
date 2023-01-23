c1762060cbc3a67cbf5e64efd9813f42e4c878ba
Iacopo Buffoni <itsmadpupp@outlook.it>
2023-01-23 08:39:23 +0100

    Completato senza bonus

--- js/script.js
+++ js/script.js
@@ -27,11 +27,17 @@ console.log(_Slides);
 let _ActiveSlide = 0;
 
 for (let i = 0; i < images.length; i ++) {
-    _Slides.innerHTML += `<img src="${images[i].image}" alt="" class="hidden">`
+    _Slides.innerHTML += `<img src="${images[i].image}" alt="" class="hidden">
+                            <div class="title hidden">
+                                <h2> ${images[i].title} </h2>
+                                <p> ${images[i].text} </p>
+                            </div>`
 }
 
 const _AllSlides = document.querySelectorAll('.slides img');
+const _AllTitles = document.querySelectorAll('.title');
 _AllSlides[_ActiveSlide].classList.remove('hidden');
+_AllTitles[_ActiveSlide].classList.remove('hidden');
 console.log(_AllSlides);
 
 const _Previous = document.getElementById('back');
@@ -40,15 +46,23 @@ const _Next = document.getElementById('next');
 _Next.addEventListener ('click',
 
     function () {
+
         console.log(_AllSlides[_ActiveSlide])
         _AllSlides[_ActiveSlide].classList.add('hidden');
-        _ActiveSlide ++;
-        _AllSlides[_ActiveSlide].classList.remove('hidden');
+        _AllTitles[_ActiveSlide].classList.add('hidden');
+
         if (_ActiveSlide == _AllSlides.length - 1) {
 
             _ActiveSlide = 0;
 
+        } else {
+
+            _ActiveSlide ++;
+
         }
+
+        _AllSlides[_ActiveSlide].classList.remove('hidden');
+        _AllTitles[_ActiveSlide].classList.remove('hidden');
 
     }
 )
@@ -57,9 +71,22 @@ _Previous.addEventListener ('click',
 
     function () {
         
+        console.log(_AllSlides[_ActiveSlide])
         _AllSlides[_ActiveSlide].classList.add('hidden');
-        _ActiveSlide --;
+        _AllTitles[_ActiveSlide].classList.add('hidden');
+
+        if (_ActiveSlide == 0) {
+
+            _ActiveSlide = _AllSlides.length - 1;
+
+        } else {
+
+            _ActiveSlide --;
+
+        }
+
         _AllSlides[_ActiveSlide].classList.remove('hidden');
+        _AllTitles[_ActiveSlide].classList.remove('hidden');
 
     }
 )
